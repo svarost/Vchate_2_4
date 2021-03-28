92532e6fb70d9a339ab05e01fe359e4f8d5fead9
svarost <sir.shvetsov@gmail.com>
2021-03-28 21:46:20 +0300

    HT_2_4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalTime;
+
 public class Controller {
+    private String userName = "Viktor";
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    void sendMsg() {
+        if(!textField.getText().isEmpty()) {
+            textArea.appendText(userName + ": " + textField.getText() + "\n");
+            textField.clear();
+            textField.requestFocus();
+        }
+    }
 }
